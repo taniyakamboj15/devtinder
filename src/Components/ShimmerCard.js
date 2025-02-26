c4097517d9d4a4927063f5cc6b566f91ef4355a7
Taniya Kamboj <taniyakamboj184@gmail.com>
2025-02-26 18:45:18 +0530

    made shimmer card responsive

--- src/Components/ShimmerCard.js
+++ src/Components/ShimmerCard.js
@@ -2,20 +2,20 @@ import React from 'react';
 
 const ShimmerCard = () => {
   return (
-    <div className='border-4 border-black rounded-lg w-fit h-fit top-20 bg-black md:mx-auto mx-4 p-4 animate-pulse'>
-      {/* Image shimmer */}
-      <div className='w-96 h-96 bg-gray-700 rounded-md'></div>
+    <div className='border-4 border-black rounded-lg w-full max-w-sm md:max-w-md lg:max-w-lg h-fit bg-black mx-auto p-4 animate-pulse'>
+      {/* Image Placeholder */}
+      <div className='w-full h-72 md:h-80 lg:h-96 bg-gray-700 rounded-md'></div>
       
-      {/* Text shimmer */}
-      <div className='mt-3 px-3'>
-        <div className='h-6 bg-gray-600 rounded w-3/4 mb-2'></div>
-        <div className='h-6 bg-gray-600 rounded w-1/4'></div>
+      {/* Text Placeholder */}
+      <div className='mt-3 space-y-2'>
+        <div className='h-6 bg-gray-700 rounded w-3/4'></div>
+        <div className='h-5 bg-gray-700 rounded w-1/4'></div>
       </div>
       
-      {/* Buttons shimmer */}
+      {/* Buttons Placeholder */}
       <div className='flex gap-5 my-5 justify-center'>
-        <div className='w-24 h-10 bg-gray-700 rounded-md'></div>
-        <div className='w-24 h-10 bg-gray-700 rounded-md'></div>
+        <div className='w-24 h-10 bg-gray-700 rounded'></div>
+        <div className='w-24 h-10 bg-gray-700 rounded'></div>
       </div>
     </div>
   );
