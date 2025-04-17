501d5f9dc3917ce27daeaac70e75cabdfc5d84db
Taniya Kamboj <taniyakamboj184@gmail.com>
2025-04-18 02:50:06 +0530

    fix

--- src/constants/socket.js
+++ src/constants/socket.js
@@ -1,5 +1,5 @@
 import { io } from "socket.io-client";
 import { BASE_URL } from "./constants";
 export const createSocketConnection = () => {
-  return io(BASE_URL, { transports: ["websocket"] });
+  return io(BASE_URL);
 };
