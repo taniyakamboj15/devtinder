1b9ded5fdceb0ff13e6f660642f55d69852b489d
Taniya Kamboj <taniyakamboj184@gmail.com>
2025-04-17 14:07:32 +0530

    bug fix

--- src/index.jsx
+++ src/index.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./Layout/App";
 import { Provider } from "react-redux";
-import appStore from "./redux/appstore";
+import appStore from "./redux/appStore";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
