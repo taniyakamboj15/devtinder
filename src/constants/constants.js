4c51597e5f5d9c3fb2273e7edf01ddb40711645f
Taniya Kamboj <taniyakamboj184@gmail.com>
2025-04-17 19:41:56 +0530

    base url added

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -1,1 +1,4 @@
-export const BASE_URL = "http://localhost:3000";
+export const BASE_URL =
+  process.env.NODE_ENV === "production"
+    ? "dev-tinder-backend-three.vercel.app"
+    : "http://localhost:3000";
